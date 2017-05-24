0d92bf5e18574862d56f131bc0539f739d96df5d
e.balashov <e.balashov@rambler-co.ru>
2017-05-24 15:40:21 +0300

    tracking only the vertical coordinate. Add auto-set 'visible' attr.

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 window.addEventListener('load', function() {
   var allFrames = document.getElementsByTagName('iframe');
 
+  for (var i = 0, len = allFrames.length; i < len; i++) {
+    allFrames[i].setAttribute('visible', false);
+  }
   sendMessage(allFrames);
 
   window.addEventListener('scroll', function() {
@@ -40,9 +43,7 @@ window.addEventListener('load', function() {
     var rect = element.getBoundingClientRect();
     var html = document.documentElement;
     var elementVisibility = rect.top >= 0
-      && rect.left >= 0
-      && rect.bottom <= (window.innerHeight || html.clientHeight)
-      && rect.right <= (window.innerWidth || html.clientWidth);
+      && rect.bottom <= (window.innerHeight || html.clientHeight);
     return elementVisibility;
   }
 });
